cb22e0d7ef0f5d75225aefb7c934649690849892
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 20:00:42 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,6 +1,19 @@
 module Dance
 
-
+  def twirl   
+  "I'm twirling!"
+  end
+  
+  def jump 
+    "look how high I'm Jumping!"
+  end
+  
+  def pirouette
+  "I'm doing a pirouette"
+  end
+  
+  def take_a_bow
+  "Thank you "
 
 
 end
